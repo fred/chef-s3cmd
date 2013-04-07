9e3b27a6ffa6277fdb8e0f32243b227d4122e3d7
Art Carey <nsfyn55@gmail.com>
2013-04-07 09:06:54 -0400

    Bugfix chef kept complaining about syntax error

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,6 @@
 #
 # Cookbook Name:: s3cmd
-# Recipe:: default
-#
+# Recipe:: default #
 # Frederico Araujo (fred.the.master@gmail.com)
 # https://github.com/fred/chef-s3cmd
 #
@@ -31,12 +30,12 @@ home_folder = `echo ~#{node['s3cmd']['user']}`.strip
 template "#{home_folder}/.s3cfg" do
   source "s3cfg.erb"
   variables(
-    access_key: node['s3cmd']['access_key'],
-    secret_key: node['s3cmd']['secret_key'],
-    gpg_passphrase: node['s3cmd']['gpg_passphrase'],
-    bucket_location: node['s3cmd']['bucket_location'],
-    https: node['s3cmd']['https'],
-    encrypt: node['s3cmd']['encrypt']
+    :access_key =>  node['s3cmd']['access_key'],
+    :secret_key =>  node['s3cmd']['secret_key'],
+    :gpg_passphrase =>  node['s3cmd']['gpg_passphrase'],
+    :bucket_location =>  node['s3cmd']['bucket_location'],
+    :https =>  node['s3cmd']['https'],
+    :encrypt =>  node['s3cmd']['encrypt']
   )
   owner node['s3cmd']['user']
   group node['s3cmd']['user']
