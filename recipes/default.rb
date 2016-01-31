442bacf74e121c70d2944234bc8724ce632e2717
Yorgos Saslis <g.saslis@gmail.com>
2016-01-31 19:42:52 +0200

    Changed the way we try to load the values for the config file attributes
    Recipe now first attempts to load values from a Chef Vault. If it fails, it reverts to the old way where we use node attributes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,16 +19,36 @@ else
   home_folder = node['etc']['passwd'][node['s3cmd']['user']]['dir']
 end
 
+include_recipe 'chef-vault'
+
+begin
+  # First try to load s3 config through Chef Vault, as the more secure way.
+  s3cfg_variables = {
+    access_key: chef_vault_item_for_environment('aws_creds', 's3_user')['access_key'],
+    secret_key: chef_vault_item_for_environment('aws_creds', 's3_user')['secret_key'],
+    gpg_passphrase: chef_vault_item_for_environment('aws_creds', 's3_user')['gpg_passphrase'],
+    bucket_location: chef_vault_item_for_environment('aws_creds', 's3_user')['bucket_location'],
+    https: chef_vault_item_for_environment('aws_creds', 's3_user')['https'],
+    encrypt: chef_vault_item_for_environment('aws_creds', 's3_user')['encrypt']
+  }
+rescue ChefVault::Exceptions::KeysNotFound, Net::HTTPServerException => e
+  if e || e.response_code == '404'
+    # if we couldn't find the data bag, revert to plain node attributes
+    s3cfg_variables = {
+      access_key: node['s3cmd']['access_key'],
+      secret_key: node['s3cmd']['secret_key'],
+      gpg_passphrase: node['s3cmd']['gpg_passphrase'],
+      bucket_location: node['s3cmd']['bucket_location'],
+      https: node['s3cmd']['https'],
+      encrypt: node['s3cmd']['encrypt']
+    }
+  end
+end
+
+
 template "#{home_folder}/.s3cfg" do
   source 's3cfg.erb'
-  variables(
-    access_key: node['s3cmd']['access_key'],
-    secret_key: node['s3cmd']['secret_key'],
-    gpg_passphrase: node['s3cmd']['gpg_passphrase'],
-    bucket_location: node['s3cmd']['bucket_location'],
-    https: node['s3cmd']['https'],
-    encrypt: node['s3cmd']['encrypt']
-  )
+  variables s3cfg_variables
   owner node['s3cmd']['user']
   group node['s3cmd']['user']
   mode 0600
